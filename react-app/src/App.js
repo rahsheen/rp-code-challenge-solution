eb8de975ddae1c6e4a85bd93903b966796c5fa34
Rahsheen Porter <rahsheen.porter@gmail.com>
2019-03-04 07:55:30 -0500

    Initial commit

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -4,18 +4,49 @@ import './App.css'
 
 const GIT_USER = ''
 const GIT_USER_URL = `https://api.github.com/users/${GIT_USER}/events`
+const SCORE_INFO = {
+  'PushEvent': 5,
+  'PullRequestReviewCommentEvent': 4,
+  'WatchEvent': 3,
+  'CreateEvent': 2,
+}
 
 export default class App extends Component {
+  state = {
+    score: null,
+    multiplier: null
+  }
+
+  calculateScore = data => data.reduce((total, {type}) => total + (SCORE_INFO[type] || 1),0)
+  multiplyScore = score => {
+    return this.state.multiplier === null ? score : this.state.multiplier * score;
+  }
+
+  getData = () => {
+    console.log("Clicked")
+    const total = 0;
+    fetch('https://api.github.com/users/dhh/events')
+    .then(response => response.json())
+    .then(data => this.setState({score: this.multiplyScore(this.calculateScore(data))}))
+    .catch(e => console.log)
+  }
+
+  onTextChange = e => {
+    this.setState({multiplier: e.target.value});
+  }
+
   render() {
     return (
       <div className="App">
         <div className="App_score">
-          n/a
+          {this.state.score == null ? 'n/a' : this.state.score}
         </div>
+        <input type="number" onChange={this.onTextChange} />
         <button
           type="button"
           className="App_button"
           children="Get Score"
+          onClick={this.getData}
         />
       </div>
     );
